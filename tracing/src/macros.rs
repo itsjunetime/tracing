0ee336aa3d08de552df37e961f838cb879ec81f8
itsjunetime <junewelker@gmail.com>
2026-09-14 07:55:24 -0500

    it compiles ent

--- tracing/src/macros.rs
+++ tracing/src/macros.rs
@@ -3247,20 +3247,7 @@ macro_rules! __tracing_log {
 #[macro_export]
 macro_rules! __tracing_log {
     ($level:expr, $callsite:expr, $value_set:expr) => {
-        use $crate::log;
-        let level = $crate::__macro_support::level_to_log($level);
-        if level <= log::max_level() {
-            let meta = $callsite.metadata();
-            let log_meta = log::Metadata::builder()
-                .level(level)
-                .target(meta.target())
-                .build();
-            let logger = log::logger();
-            if logger.enabled(&log_meta) {
-                $crate::__macro_support::__tracing_log(meta, logger, log_meta, $value_set)
-            }
-        }
-        // $crate::__macro_support::__tracing_log_macro_replacement($level, $callsite.metadata(), $value_set)
+        $crate::__macro_support::__tracing_log_macro_replacement($level, $callsite.metadata(), $value_set)
     };
 }
 
